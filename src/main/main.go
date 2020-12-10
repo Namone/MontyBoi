fcac393f18537cd98f459a8041ae24fdca3e765f
Alex Nordhausen <anorthhouse@gmail.com>
2020-12-10 14:37:37 -0700

    Compiling

--- src/main/main.go
+++ src/main/main.go
@@ -23,7 +23,7 @@ var (
 func main() {
 	port := os.Getenv("PORT")
 	// Don't forget to omit the trailing /!
-	serverName := "web:" + port
+	serverName := "host.docker.internal:" + port
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 		data := MontyBoi{
@@ -93,5 +93,5 @@ func main() {
 	p := os.Getenv("PORT")
 	fmt.Print("Started running on web:" + p + "\n")
 
-	log.Fatal(http.ListenAndServe("web:"+os.Getenv("PORT"), nil))
+	log.Fatal(http.ListenAndServe("host.docker.internal:"+os.Getenv("PORT"), nil))
 }
